449a6b2dd584bd5338be05baecf74bbd02555ed9
leonardo reis <leonardo92reis@gmail.com>
2024-05-16 19:23:14 -0300

    update: finalizando detalhes

--- app.ts
+++ app.ts
@@ -1,9 +1,19 @@
 import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
+import { FunctionaryAccount } from './class/FunctionaryAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
-console.log(peopleAccount)
-peopleAccount.deposit()
+  peopleAccount.deposit(20)
+  peopleAccount.withdraw(10)
+  console.log(peopleAccount)
+  
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
-console.log(companyAccount)
+  companyAccount.deposit(10)
+  companyAccount.withdraw(10)
+  companyAccount.getLoan(500)
+  console.log(companyAccount)
+
+  const functionaryAccout: FunctionaryAccount = new FunctionaryAccount('leo', 5)
+  functionaryAccout.depositWithBonus(100)
+  functionaryAccout.withdraw(50)
+  console.log(functionaryAccout)
